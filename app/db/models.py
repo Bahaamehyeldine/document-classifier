ad53902adea7c715334edd3a938747a6d648cf92
bahaamehyeldine <mehyeldinebahaa@gmail.com>
2026-08-19 12:30:53 +0300

    Add User model and initial Alembic migration

---
+++ app/db/models.py
@@ -0,0 +1,12 @@
+import uuid
+
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    __tablename__ = "user"
